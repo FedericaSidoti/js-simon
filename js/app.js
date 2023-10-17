7cdd19134ba44145ab9f697fa63ed5ddde9033b2
FedericaSidoti <federicasidoti21@gmail.com>
2023-10-17 16:06:29 +0200

    ADD- alert, array and prompt generation

--- js/app.js
+++ js/app.js
@@ -1,8 +1,38 @@
 //- creare la funzione per creare 5 numeri casuali
 
 //- stampare in un alert i 5 numeri
-//- setTimeOut a 30 secondi, dopo di che
-//    - ciclo for da 5 iterazioni che stampa un prompt
+const numberMemory = 5
+const fiveNumbersArray = getFiveNumbers(1, 50, numberMemory)
+//console.log(fiveNumbers)
+alert(`Ricorda questi 5 numeri: ${fiveNumbersArray}`)
+//- setTimeOut a 30 secondi, dopo di che FUNZIONE
+
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); 
+}
+
+function getFiveNumbers(minRange, maxRange, numberOfNumbers) {
+    const arrayNumbers = [];
+
+    while (arrayNumbers.length < numberOfNumbers) {
+        const n = getRandomInt(minRange, maxRange);
+        if (!arrayNumbers.includes(n)) {
+            arrayNumbers.push(n);
+        }
+    }
+    return arrayNumbers;
+}
+
+function memoryGame (numberOfMemoryNumbers) {
+    //    - ciclo for da 5 iterazioni che stampa un prompt
+    for(let i = 0; i < numberOfMemoryNumbers; i++) {
+        prompt('inserisci qui un numero che ti ricordi')
+    }
+}
+
 //    - PER OGNI prompt recuperare il valore e confrontarlo con l'array di numeri 
 //    - SE è presente
 //        - allora aumenta il punteggio di 1 
