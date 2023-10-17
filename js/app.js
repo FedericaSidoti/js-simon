87b6791eae4de4021286b97d1163892bf85823e3
FedericaSidoti <federicasidoti21@gmail.com>
2023-10-17 15:28:59 +0200

    ADD- files

---
+++ js/app.js
@@ -0,0 +1,8 @@
+//- creare la funzione per creare 5 numeri casuali
+
+//- stampare in un alert i 5 numeri
+//- setTimeOut a 30 secondi, dopo di che
+//    - ciclo for da 5 iterazioni che stampa un prompt
+//    - PER OGNI prompt recuperare il valore e confrontarlo con l'array di numeri 
+//    - SE è presente
+//        - allora aumenta il punteggio di 1 
